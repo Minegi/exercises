4ac42500979661b87c7956fa074a59d7a794445c
Rexa <zhaoweic18@gmail.com>
2017-03-26 09:09:30 +0900

    Catch a sign in & sign up error case

--- react-native-study/react-native/auth/src/components/LoginForm.js
+++ react-native-study/react-native/auth/src/components/LoginForm.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '', password: '' };
+    state = { email: '', password: '', error: '' };
+    onButtonPress() {
+        const { email, password, error } = this.state;
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication Failed.' });
+                    });
+            });
+    }
 
     render() {
         return (
@@ -27,8 +39,12 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
-                    <Button>
+                    <Button onPress={this.onButtonPress.bind(this)}>
                         Login
                     </Button>
                 </CardSection>
@@ -36,5 +52,14 @@ class LoginForm extends Component {
         );
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red',
+
+    }
+};
 
 export default LoginForm;
