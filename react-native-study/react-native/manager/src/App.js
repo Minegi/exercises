91fe29cad4faac91558ccbcd68d8751195d44a13
Rexa <zhaoweic18@gmail.com>
2017-04-03 23:00:57 +0900

    Add components & refactor ActionCreators
    Install 'react-native-router-flux' for navigating components
    Add Router & Scenes component for page navigation
    Refactor ActionCreator
    Add EmployeeCreate, EmployeeList components
    Add a EmployeeFormReducer

--- react-native-study/react-native/manager/src/App.js
+++ react-native-study/react-native/manager/src/App.js
@@ -5,7 +5,7 @@ import firebase from 'firebase';
 import ReduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 export default class App extends Component {
     componentWillMount() {
@@ -23,7 +23,7 @@ export default class App extends Component {
         const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
         return (
             <Provider store={store}>
-                <LoginForm />
+                <Router />
             </Provider>
         );
     }
